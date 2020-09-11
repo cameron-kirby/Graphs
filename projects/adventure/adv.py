4fc37447369208b4da926a9920dc18efe3e3a1ea
Cameron Kirby <cameronevankirby@gmail.com>
2020-09-11 11:28:09 -0500

    Finished Sprint

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -2,9 +2,11 @@ from room import Room
 from player import Player
 from world import World
 
+import sys
+
 import random
 from ast import literal_eval
-
+from util import Queue, Stack
 # Load world
 world = World()
 
@@ -15,7 +17,6 @@ world = World()
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
-
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
@@ -23,18 +24,113 @@ world.load_graph(room_graph)
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
-
+player = Player(world.starting_room) ## for test
+player_2 = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
-
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
+# Work Space
+################################################################################
+
+def bfs(start):
+    # Make Queue
+    q = Queue()
+    q.enqueue([start])
+    visited = set()
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        if v not in visited:
+            if '?' in list(traversial_graph[v].values()):
+                return path
+            visited.add(v)
+            for neighbor in list(traversial_graph[v].values()):
+                new_path = list(path)
+                new_path.append(neighbor)
+                q.enqueue(new_path)
+    return None
+
+def reverse_dict(dictionary):
+    return {v: k for k, v in dictionary.items()}
+
+# Create a dictionary of rooms
+traversial_graph = {
+    player_2.current_room.id:
+        dict.fromkeys(player_2.current_room.get_exits(), '?')
+}
+
+done = False
+
+while done == False:
+    current = player_2.current_room
+
+    # Pick a random unexplored direction
+    possible_moves = [i for i in traversial_graph[current.id]\
+                        if traversial_graph[current.id][i] == '?']
+
+    if len(possible_moves) > 0:
+        move = random.choice(possible_moves)
+        # Travel to that room and log the direction
+        previous_room_id = current.id
+        player_2.travel(move)
+        current = player_2.current_room
+        traversial_graph[previous_room_id][move] = current.id
+        traversal_path.append(move)
+
+        # If the room isn't in traversial_graph
+        if current.id not in list(traversial_graph.keys()):
+            # Add room to the traversial_graph
+            traversial_graph[current.id] = dict.fromkeys(
+                                               current.get_exits(), '?')
+
+        if move == 'n':
+            traversial_graph[current.id]['s'] = previous_room_id
+        elif move == 's':
+            traversial_graph[current.id]['n'] = previous_room_id
+        elif move == 'e':
+            traversial_graph[current.id]['w'] = previous_room_id
+        elif move == 'w':
+            traversial_graph[current.id]['e'] = previous_room_id
+
+    else:
+        # find the shortest path to a room with an unexplored path
+        short_path = bfs(current.id)
+        if short_path == None:
+            break
+            
+        directed_path = []
+        for i in range(len(short_path) + 1):
+            if i + 1 < len(short_path):
+                direction = reverse_dict(
+                    traversial_graph[short_path[i]])[short_path[i+1]]
+                directed_path.append(direction)
+
+        # Travel along that path and log the direction
+        for direction in directed_path:
+            previous_room_id = current.id
+            player_2.travel(direction)
+            traversal_path.append(direction)
+
+
+    # Iteration
+    values = []
+    for adj_dict in list(traversial_graph.values()):
+        for i in list(adj_dict.values()):
+            values.append(i)
+
+    if '?' in values or len(traversial_graph) != len(room_graph):
+        done = False
+    else:
+        done = True
+
+################################################################################
+
 
 for move in traversal_path:
     player.travel(move)
@@ -51,6 +147,7 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
+"""
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
@@ -60,3 +157,4 @@ while True:
         break
     else:
         print("I did not understand that command.")
+"""
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,42 +13,72 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create Queue
+        q = Queue()
+        # Add starting vertex to queue & make visited an empty set
+        q.enqueue(starting_vertex)
+        visited = set()
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        visited = set()
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
 
-    def dft_recursive(self, starting_vertex):
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        
+        visited.add(starting_vertex)
+        print(starting_vertex)
+        for child_vert in self.vertices[starting_vertex]:
+            if child_vert not in visited:
+                self.dft_recursive(child_vert, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -56,7 +86,24 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        q = Queue()
+        q.enqueue([starting_vertex])
+        visited = set()
+
+        while q.size() > 0:
+            v = q.dequeue()
+
+            last = v[-1]
+
+            if last not in visited:
+                if last == destination_vertex:
+                    return v
+                else:
+                    visited.add(last)
+                    for neighbor in self.vertices[last]:
+                        path = v[:]
+                        path.append(neighbor)
+                        q.enqueue(path)
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -64,9 +111,26 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        s = Stack()
+        s.push([starting_vertex])
+        visited = set()
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+        while s.size() > 0:
+            v = s.pop()
+
+            last = v[-1]
+
+            if last not in visited:
+                if last == destination_vertex:
+                    return v
+                else:
+                    visited.add(last)
+                    for neighbor in self.vertices[last]:
+                        path = v[:]
+                        path.append(neighbor)
+                        s.push(path)
+
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -74,7 +138,31 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # Check if the node has been visited
+        if visited is None:
+            visited = set()
+
+        # Check to see if this is the initial start
+        if path is None:
+            path = []
+
+        # Mark it as visited
+        visited.add(starting_vertex)
+
+        # append the starting_vertex onto the path
+        path = path + [starting_vertex]
+
+        # Check if we are at the target
+        if starting_vertex == destination_vertex:
+            return path
+
+        # Call dft_recursive on neighbors
+        for child_vertex in self.get_neighbors(starting_vertex):
+            if child_vertex not in visited:
+                new_path = self.dfs_recursive(child_vertex, destination_vertex, visited, path)
+                if new_path:
+                    return new_path
+        return None
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
